5690b73586b39dc0600dcf56e0808f6abcc66544
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-10 11:25:06 +0530

    Implemented register login logic

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,14 +2,70 @@ const express = require("express");
 const User = require("../models/userModel");
 
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 router.post("/register", async (req, res) => {
+    try {
+        
+        // const newUser = await User.create({
+        //     name: req.body.name,
+        //     email: req.body.email,
+        //     password: req.body.password
+        // });
+        const userExists = await User.findOne({email: req.body.email});
+        // if (userExists){
+        //     res.send({
+        //         success: false,
+        //         message: "User already exists"
+        //     })
+        // }
+        // const newUser = new User(req.body);
+        // await newUser.save();
+        // return res.status(201).json(newUser);
 
+        // Password Security
+        // Generate a salt
+        const salt = await bcrypt.genSalt(10);
+        // Password hashing using bcrypt
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hashedPassword;
+        const newUser = new User(req.body);
+        await newUser.save();
+        return res.status(201).json(newUser);
 
+    } catch (err) {
+        res.json(err);
+    }
 });
 
 router.post("/login", async (req, res) => {
+  // Check if the email is registered
+  // Check if the password matches
   
+  // Check if the email exixts in the database 
+  try {
+    const user = await User.findOne({email: req.body.email});
+    if (!user){
+        res.send({
+            success: false,
+            message: "User does not exist, please register."
+        })
+    }
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword){
+        return res.send({
+            success: false,
+            message: "Invalid Password"
+        })
+    }
+    res.send({
+        success: true,
+        message: "User logged in."
+    })
+  } catch (err) {
+    res.json(err);
+  }
+
 });
 
 
