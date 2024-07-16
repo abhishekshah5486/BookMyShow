0e521b9538b2fb25bbd831da96491bf892eb1d2c
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-16 14:19:58 +0530

    Code refactor

--- client/src/Components/ProtectedRoute.js
+++ client/src/Components/ProtectedRoute.js
@@ -2,21 +2,79 @@ import React, { useEffect, useState } from "react";
 import { GetCurrentUser } from "../calls/users";
 import { useNavigate } from "react-router-dom";
 import { message, Layout, Menu } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { hideLoading, showLoading } from "../redux/loaderSlice";
 import { Header } from "antd/es/layout/layout";
-import { HomeOutlined, LogoutOutlined, ProfileOutlined, UserOutlined } from "@ant-design/icons";
+import {
+  HomeOutlined,
+  LogoutOutlined,
+  ProfileOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import { setUser } from "../redux/userSlice";
 
 function ProtectedRoute({ children }) {
-  const [user, setUser] = useState(null);
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const navItems = [
+    {
+      label: "Home",
+      icon: <HomeOutlined />,
+    },
+
+    {
+      label: `${user ? user.name : ""}`,
+      icon: <UserOutlined />,
+      children: [
+        {
+          label: (
+            <span
+            onClick={() => {
+              if (user.role === 'admin') {
+                navigate("/admin");
+              } else if (user.role === 'partner') {
+                navigate("/partner");
+              } else {
+                navigate("/profile");
+              }
+            }}
+            >
+              My Profile
+            </span>
+          ),
+          icon: <ProfileOutlined />,
+        },
+
+        {
+          label: (
+            <Link
+              to="/login"
+              onClick={() => {
+                localStorage.removeItem("token");
+              }}
+            >
+              Log Out
+            </Link>
+          ),
+          icon: <LogoutOutlined />,
+        },
+      ],
+    },
+  ];
 
   const getValidUser = async () => {
     try {
+      dispatch(showLoading());
       const response = await GetCurrentUser();
-      setUser(response.data);
+      console.log(response)
+      dispatch(setUser(response.data));
+      dispatch(hideLoading());
       // Hide Loader
     } catch (error) {
-      setUser(null);
+      dispatch(setUser(null));
       message.error(error.message);
     }
   };
@@ -27,56 +85,34 @@ function ProtectedRoute({ children }) {
     } else {
       navigate("/login");
     }
-  }, [navigate]);
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    navigate("/login");
-  };
-
-  const navItems = [
-    {
-      label: <Link to="/">Home</Link>,
-      icon: <HomeOutlined />,
-    },
-    user && {
-      label: user.name,
-      icon: <UserOutlined />,
-      children: [
-        {
-          label: <Link to="/profile">My Profile</Link>,
-          icon: <ProfileOutlined />,
-        },
-        {
-          label: <span onClick={handleLogout}>Log Out</span>,
-          icon: <LogoutOutlined />,
-        },
-      ],
-    },
-  ].filter(Boolean);
+  }, []);
 
   return (
-    <Layout>
-      <Header
-        className="d-flex justify-content-between"
-        style={{
-          position: "sticky",
-          top: 0,
-          zIndex: 1,
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <h3 className="demo-logo text-white m-0" style={{ color: "white" }}>
-          Book My Show
-        </h3>
-        <Menu theme="dark" mode="horizontal" items={navItems} />
-      </Header>
-      <div style={{ padding: 24, minHeight: 380, background: "#fff" }}>
-        {children}
-      </div>
-    </Layout>
+    user && (
+      <>
+        <Layout>
+          <Header
+            className="d-flex justify-content-between"
+            style={{
+              position: "sticky",
+              top: 0,
+              zIndex: 1,
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <h3 className="demo-logo text-white m-0" style={{ color: "white" }}>
+              Book My Show
+            </h3>
+            <Menu theme="dark" mode="horizontal" items={navItems} />
+          </Header>
+          <div style={{ padding: 24, minHeight: 380, background: "#fff" }}>
+            {children}
+          </div>
+        </Layout>
+      </>
+    )
   );
 }
 
