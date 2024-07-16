e695d34665f3eeeb05513ce867bc9ce4abe4c162
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-17 00:00:31 +0530

    Implemented bookingModel.js

--- server/index.js
+++ server/index.js
@@ -5,15 +5,25 @@ require('dotenv').config();
 
 const dbConfig = require('./config/dbConfig')
 
+
+
+
 const app = express()
 
 const userRoutes = require('./routes/userRoutes')
+const movieRoute = require('./routes/movieRoutes')
+const theatreRoute = require('./routes/theatreRoute')
+const showRoute = require('./routes/showRoute')
+const bookingRoute = require('./routes/bookingRoute')
 
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/users' , userRoutes)
-
+app.use('/api/movies' , movieRoute)
+app.use('/api/theatres' , theatreRoute )
+app.use('/api/shows' , showRoute )
+app.use('/api/bookings' , bookingRoute )
 
 
 const PORT = process.env.PORT || 8081
@@ -24,3 +34,7 @@ const PORT = process.env.PORT || 8081
 app.listen(PORT , ()=>{
     console.log("server running")
 })
+
+
+
+
